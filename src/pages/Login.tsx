2aa9669e782d7b6006661dbd9d0386ef1351a822
Neto Jocelino <netoj96@live.com>
2023-01-23 12:41:01 -0300

    use GuestWizard at Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,13 +3,13 @@ import { useState } from 'react'
 import Login, { User } from '../utils/login'
 import { IsLogged } from '../utils/ReactActions'
 
-import Header from '../components/Header'
 import Input from '../components/Input'
+import GuestWizard from '../wizards/Gest'
 
 export default function () {
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
-    const [errorMessage , setErrorMessage]: [ string | null, any ] = useState(null)
+    const [errorMessage , setErrorMessage]: [ string | undefined, any ] = useState(undefined)
 
     IsLogged()
         .then((user) => {
@@ -60,52 +60,40 @@ export default function () {
     }
 
     return (
-        <div className='w-screen h-screen items-center flex flex-col overscroll-none'>
-            <Header title='BR Gaming Users' />
+        <GuestWizard
+            title='Entre na sua conta'
+            errorMessage={errorMessage}
+        >
+            <Input
+                label='Login'
+                type='text'
+                placeholder='Digite seu login'
+                inputValue={login}
+                inputHandler={handlerLogin}
+            />
 
-            <div className="w-full h-1/2 flex flex-col px-5 sm:w-1/2 sm:px-0">
+            <Input
+                label='Senha'
+                type='password'
+                placeholder='Digite sua senha'
+                inputValue={password}
+                inputHandler={handlerPassword}
+            />
 
-                <h2 className="text-center text-3xl text-custom-100 w-full mt-20">Entre na sua conta</h2>
-
-                { errorMessage && <div
-                    className='w-full p-2 rounded-md text-center font-semibold text-custom-800 bg-custom-200'
-                >
-                    { errorMessage }
-                </div>}
-
-                <form className="w-full h-3/4 flex justify-center items-center flex-col space-y-5">
-                    <Input
-                        label='Login'
-                        type='text'
-                        placeholder='Digite seu login'
-                        inputValue={login}
-                        inputHandler={handlerLogin}
-                    />
-
-                    <Input
-                        label='Senha'
-                        type='password'
-                        placeholder='Digite sua senha'
-                        inputValue={password}
-                        inputHandler={handlerPassword}
-                    />
-
-                    <div className="w-full flex justify-center items-center">
-                        <button
-                            className="w-full bg-custom-200 hover:bg-custom-300 p-2 rounded-md text-slate-50 font-semibold"
-                            type="submit"
-                            onClick={loginEvent}
-                        >Enviar</button>
-                    </div>
-
-                    <div className='w-full flex justify-center items-center'>
-                        <a
-                            className='text-custom-100 hover:text-custom-200 visited:text-custom-200 '
-                            href='#create'
-                        >Criar conta</a>
-                    </div>
-                </form>
+            <div className="w-full flex justify-center items-center">
+                <button
+                    className="w-full bg-custom-200 hover:bg-custom-300 p-2 rounded-md text-slate-50 font-semibold"
+                    type="submit"
+                    onClick={loginEvent}
+                >Enviar</button>
             </div>
-        </div>
+
+            <div className='w-full flex justify-center items-center'>
+                <a
+                    className='text-custom-100 hover:text-custom-200 visited:text-custom-200 '
+                    href='#create'
+                >Criar conta</a>
+            </div>
+        </GuestWizard>
     )
 }
