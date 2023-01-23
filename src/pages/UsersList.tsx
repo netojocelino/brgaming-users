077da8cba9fbcbf9aa1e354606c106c4ede9184c
Neto Jocelino <netoj96@live.com>
2023-01-23 11:05:45 -0300

    add menu to 'userlist' page

--- src/pages/UsersList.tsx
+++ src/pages/UsersList.tsx
@@ -41,6 +41,10 @@ export default function () {
         setUser(userData)
     }, [])
 
+    const GetBackgroundColor = (color: string) => ({
+        backgroundColor: color
+    })
+
     return user === null
         ? ( <div
             className='flex w-screen h-screen justify-center items-center text-center font-semibold text-custom-800 bg-custom-200'
@@ -60,61 +64,76 @@ export default function () {
                 </h1>
             </div>
 
-            <div className="w-full h-1/2 flex flex-col px-5 sm:w-1/2 sm:px-0">
+            <div className='flex flex-row w-full h-full'>
+                <div className='w-1/6 mr-10 h-full flex flex-col border-r border-blue-300' style={GetBackgroundColor(user.color)}>
+                    <h1 className='text-custom-200 text-center text-2xl mt-10'>{ GetRole(user.role) }</h1>
 
-                <h2 className="text-center text-3xl text-custom-100 w-full my-20">Entre na sua conta</h2>
+                    <nav className='w-full py-20'>
+                        <ul>
+                            <li className='w-full mb-5 text-custom-300 border-b-2 border-custom-100 text-center bg-clip-text bg-gradient-to-r from-black to bg-slate-500 font-semibold transition duration-100 hover:border-custom-300'><a href='#users'>Usuários</a></li>
+                            <li className='w-full mb-5 text-custom-300 border-b-2 border-custom-100 text-center bg-clip-text bg-gradient-to-r from-black to bg-slate-500 font-semibold transition duration-100 hover:border-custom-300'><a href='#store'>Minha Loja</a></li>
+                            <li className='w-full mb-5 text-custom-300 border-b-2 border-custom-100 text-center bg-clip-text bg-gradient-to-r from-black to bg-slate-500 font-semibold transition duration-100 hover:border-custom-300'><a href='#operations'>Operações</a></li>
+                        </ul>
+                    </nav>
+                </div>
 
-                { errorMessage && <div
-                    className='w-full p-2 rounded-md text-center font-semibold text-custom-800 bg-custom-200'
-                >
-                    { errorMessage }
-                </div>}
+                <div className="w-2/3 h-1/2 flex flex-col px-5 sm:w-1/2 sm:px-0">
 
-                <table className='border-collapse table-auto w-full border-spacing-2 border border-slate-300 rounded shadow'>
-                    <thead>
-                        <tr role='row'>
-                            <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>id</th>
-                            <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>nome</th>
-                            <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>nome de usuário</th>
-                            <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>número de contato</th>
-                            <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>permissão</th>
-                            <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>cor</th>
-                            <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>ação</th>
-                        </tr>
-                    </thead>
+                    <h2 className="text-center text-3xl text-custom-100 w-full my-20">Entre na sua conta</h2>
 
-                    <tbody className='bg-white dark:bg-slate-800'>
-                        {
-                            ListUsers()
-                            .map((user) => (
-                                <tr role='row' key={user._id.toString()}>
-                                    <th scope='row'>{ user._id.toString() }</th>
-                                    <th className='text-left' scope='row'>{ user.name }</th>
-                                    <th className='text-left font-normal' scope='row'>{ user.login }</th>
-                                    <th className='text-left font-normal' scope='row'>{ user.phone_number }</th>
-                                    <th className='text-left font-normal' scope='row'>{ GetRole(user.role) }</th>
-                                    <th className='text-left font-normal' scope='row'>
-                                        <input type="color" value={user.color} disabled />
-                                    </th>
-                                    <th className='text-right font-normal text-custom-200 cursor-pointer flex flex-row justify-around' scope='row'>
-                                        { false &&
-                                            <span className='hover:text-custom-300 text-xl border rounded-full border-custom-700 p-2 hover:bg-slate-200'>
-                                                <NotePencil alt='Editar' />
-                                            </span>
-                                        }
-                                        { CanRemove() &&     
-                                            <span className='hover:text-custom-300 text-xl border rounded-full border-custom-700 p-2 hover:bg-slate-200'>
-                                                <Recycle alt='Remover'/>
-                                            </span>
-                                        }
-                                    </th>
-                                </tr>
-                            ))
+                    { errorMessage && <div
+                        className='w-full p-2 rounded-md text-center font-semibold text-custom-800 bg-custom-200'
+                    >
+                        { errorMessage }
+                    </div>}
 
-                        }
-                    </tbody>
-                </table>
+                    <table className='border-collapse table-auto w-full border-spacing-2 border border-slate-300 rounded shadow'>
+                        <thead>
+                            <tr role='row'>
+                                <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>id</th>
+                                <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>nome</th>
+                                <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>nome de usuário</th>
+                                <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>número de contato</th>
+                                <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>permissão</th>
+                                <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>cor</th>
+                                <th className='border-b dark:border-slate-400 font-semibold p-4 py-2 text-left' scope='col'>ação</th>
+                            </tr>
+                        </thead>
+
+                        <tbody className='bg-white dark:bg-slate-800'>
+                            {
+                                ListUsers()
+                                .map((user) => (
+                                    <tr role='row' key={user._id.toString()}>
+                                        <th scope='row'>{ user._id.toString() }</th>
+                                        <th className='text-left' scope='row'>{ user.name }</th>
+                                        <th className='text-left font-normal' scope='row'>{ user.login }</th>
+                                        <th className='text-left font-normal' scope='row'>{ user.phone_number }</th>
+                                        <th className='text-left font-normal' scope='row'>{ GetRole(user.role) }</th>
+                                        <th className='text-left font-normal' scope='row'>
+                                            <input type="color" value={user.color} disabled />
+                                        </th>
+                                        <th className='text-right font-normal text-custom-200 cursor-pointer flex flex-row justify-around' scope='row'>
+                                            { false &&
+                                                <span className='hover:text-custom-300 text-xl border rounded-full border-custom-700 p-2 hover:bg-slate-200'>
+                                                    <NotePencil alt='Editar' />
+                                                </span>
+                                            }
+                                            { CanRemove() &&
+                                                <span className='hover:text-custom-300 text-xl border rounded-full border-custom-700 p-2 hover:bg-slate-200'>
+                                                    <Recycle alt='Remover'/>
+                                                </span>
+                                            }
+                                        </th>
+                                    </tr>
+                                ))
+
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
+
         </div>
     )
 }
