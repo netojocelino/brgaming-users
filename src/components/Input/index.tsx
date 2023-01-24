47f76c63179a0c7f8239ff063eefe5c2d3d1788b
Neto Jocelino <netoj96@live.com>
2023-01-23 22:09:39 -0300

    add disabled attribute to input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,7 @@
 interface InputProps {
     label: string
     type: string
+    disabled?: boolean | false
     placeholder?: string
     inputValue: any
     inputHandler: any 
@@ -16,6 +17,7 @@ export default function (props: InputProps) {
                 value={props.inputValue}
                 onChange={props.inputHandler}
                 type={props.type}
+                disabled={props.disabled}
                 placeholder={props?.placeholder}
                 className="p-1 text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md ring-1 ring-custom-600 shadow-sm w-3/4"
             />
